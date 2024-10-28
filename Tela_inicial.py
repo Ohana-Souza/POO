dc22141d3821a35deed85df2ed63ee7e951551e4
Ohana <ohanasouza04@gmail.com>
2024-10-28 20:25:58 -0300

    Testes iniciais para aprender a usar o Tkinter no vscode

---
+++ Tela_inicial.py
@@ -0,0 +1,14 @@
+from tkinter import * 
+
+janela_numeroum = Tk()
+
+janela_numeroum.title("janelainicio")
+
+janela_numeroum.geometry("550x350+205+205")
+
+janela_numeroum.resizable(True,True)
+
+janela_numeroum['bg'] = "green"
+
+
+janela_numeroum.mainloop()
