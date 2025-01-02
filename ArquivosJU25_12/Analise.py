6bdffd2c2935b60ddfe013420d051711acc45469
Ohana-Souza <ohanasouza04@gmail.com>
2025-01-02 18:35:01 -0300

    exclusão de códigos não funcionais e inserção somente dos códigos FUNCIONAIS

--- ArquivosJU25_12/Analise.py
+++
@@ -1,9 +0,0 @@
-from Interface_Insulina import InterfaceInsulina
-from typing import Type
-
-class Analise(InterfaceInsulina):
-    def fazCalculoDosagem(self, insulina: Type[InterfaceInsulina]):
-        pass
-
-    def fazMostraDosagem(self, insulina: Type[InterfaceInsulina]):
-        pass
--- ArquivosJU25_12/helpers.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-# Função para mudar de tela
-def mudar_tela(nova_tela, root, *args):
-    for widget in root.winfo_children():
-        widget.destroy()
-    nova_tela(root, *args)
