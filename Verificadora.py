50443c4aff3b64ea7cbb22218a90730732688fa9
juliadinizdti <julia.rodrigues@dtidigital.com.br>
2025-01-09 14:21:40 -0300

    COMMIT - Uma mudança esqueci de dar staged rs

---
+++ Verificadora.py
@@ -0,0 +1,13 @@
+
+
+class Verificadora:
+    @staticmethod
+    def verificar_numero_coerente(valor, tipo="float"):
+        try:
+            if tipo == "float":
+                return float(valor)
+            elif tipo == "int":
+                return int(valor)
+        except ValueError:
+            raise ValueError("Valor inválido. Insira um número válido.")
+
